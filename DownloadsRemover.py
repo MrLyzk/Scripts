8cca0f37914d0557384cae7df17a35970619c039
jakub <kkrusicki@gmail.com>
2022-11-29 19:34:04 +0100

    Most basic but working version

--- DownloadsRemover.py
+++ DownloadsRemover.py
@@ -1,10 +1,30 @@
 import os
 
-folder = '/home/jakub/Downloads'
-files = 0
-for filename in os.listdir(folder):
-    files += 1
-    print(filename)
+directory = '/home/jakub/Downloads'
+files = []
 
-# os.remove('/home/jakub/Downloads/steam_latest.deb')
-print(files)
+
+def display(folder):
+    num = 0
+    for f in os.listdir(folder):
+        files.append(f)
+        num += 1
+    return num
+
+
+move = True
+
+num_of_files = display(directory)
+
+while move:
+    if num_of_files == 0: 
+        print("There is no files.")
+        move = False
+    else:
+        for filename in files:
+            file = '/home/jakub/Downloads/' + filename
+            print(file)
+            # os.remove(file)
+            num_of_files -= 1
+
+print(num_of_files)
