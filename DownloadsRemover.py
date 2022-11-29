6c1f12b2031bb9d2e22ef5df8016ee70dc654cc6
jakub <kkrusicki@gmail.com>
2022-11-29 20:04:43 +0100

    it doesn't work yet

--- DownloadsRemover.py
+++ DownloadsRemover.py
@@ -2,9 +2,10 @@ import os
 
 directory = '/home/jakub/Downloads'
 files = []
+files_to_spare = []
 
 
-def display(folder):
+def counter(folder):
     num = 0
     for f in os.listdir(folder):
         files.append(f)
@@ -12,18 +13,31 @@ def display(folder):
     return num
 
 
+def ask():
+    for i in files:
+        print(i)
+
+    decision = input("Do you want to remove all the given files? (y/n):\n")
+
+    if decision == "n":
+        files_to_spare.append(input("Write the exact name of the file to be spared, "
+                                    "otherwise it'll be deleted permanently: "))
+
+
 move = True
 
-num_of_files = display(directory)
+num_of_files = counter(directory)
 
 while move:
+    ask()
     if num_of_files == 0: 
         print("There is no files.")
         move = False
     else:
         for filename in files:
-            file = '/home/jakub/Downloads/' + filename
-            print(file)
+            if filename not in files_to_spare:
+                file = '/home/jakub/Downloads/' + filename
+                print(file)
             # os.remove(file)
             num_of_files -= 1
 
